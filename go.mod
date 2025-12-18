3dbae9146915bc88c52d8aa710840e1ac760f9ba
yuli <yulianingsih180705@gmail.com>
2025-12-18 20:14:53 +0800

    develop: update

--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	github.com/go-sql-driver/mysql v1.9.3
 )
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+)
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,12 +2,21 @@ package routes
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 
 	"submission_hmc/internal/handler"
 	mw "submission_hmc/internal/middleware"
 )
 
 func RegisterBookRoutes(r chi.Router, h *handler.BookHandler) {
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Content-Type", "X-ROLE"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
 	r.Route("/books", func(r chi.Router) {
 		r.Get("/", h.GetBooks)
 		r.Get("/{id}", h.GetBookByID)
