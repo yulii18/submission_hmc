cb6d5ba8a9c1a09e7062d585f746cb72124ec435
larafauzia <larafzia26@gmail.com>
2025-12-19 20:40:58 +0800

    checkpoint login sudah bisa, sudah ada middleware, crud buku

--- cmd/main.go
+++ cmd/main.go
@@ -1,53 +1,40 @@
-
 package main
 
 import (
-	"database/sql"
 	"log"
 	"net/http"
-
+	"os"
+	"submission_hmc/internal/config"
 	"submission_hmc/internal/handler"
-	"submission_hmc/internal/middleware"
 	"submission_hmc/internal/repository"
+	"submission_hmc/internal/routes"
 	"submission_hmc/internal/service"
 
 	"github.com/go-chi/chi/v5"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// Connect ke database
-	db, err := sql.Open(
-		"mysql",
-		"root:@tcp(127.0.0.1:3306)/perpustakaan?parseTime=true",
-	)
+	jwtKey := []byte(os.Getenv("JWT_SECRET"))
+
+	db, err := config.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
 	userRepo := repository.NewUserRepository(db)
-	authService := service.NewAuthService(userRepo, []byte("secret_key"))
+	bookRepo := repository.NewBookRepository(db)
+
+	authService := service.NewAuthService(userRepo, jwtKey)
+	bookService := service.NewBookService(bookRepo)
+
 	authHandler := handler.NewAuthHandler(authService)
+	bookHandler := handler.NewBookHandler(bookService)
+	adminHandler := handler.NewAdminHandler()
 
 	r := chi.NewRouter()
 
-	
-	r.Post("/auth/login", authHandler.Login)
+	routes.RegisterRoutes(r, authHandler, bookHandler, adminHandler, jwtKey)
 
-	
-	r.Route("/user", func(r chi.Router) {
-		r.Use(middleware.JWTMiddleware([]byte("secret_key"))) 
-		r.Get("/profile", authHandler.Profile)               
-	})
-
-	
-	r.Route("/admin", func(r chi.Router) {
-	r.Use(middleware.JWTMiddleware([]byte("secret_key")))
-	r.Use(middleware.AdminOnly) // HARUS dikenali sekarang
-	r.Get("/", authHandler.AdminDashboard)
-    })
-
-	log.Println("server running at :8080")
+	log.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
